f2481f54c5561b64ecbbc299b7f85a3944f15799
Gerardo <gerardocolina@gmail.com>
2023-04-10 20:29:41 -0400

    Initial setup and initial folder structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
